c8d76dfe9defb35b8844b15869877ec17cb2f07a
Bird0Ut <123623881+Bird0Ut@users.noreply.github.com>
2023-12-18 17:53:40 +0100

    amelioration

--- plantumlprinter.py
+++ plantumlprinter.py
@@ -12,8 +12,11 @@ def get_current_directory():
 
 def generate_images(filename, outdir, server='http://www.plantuml.com/plantuml/img/'):
     pl = plantuml.PlantUML(server)
-    result = pl.processes_file(filename, outfile=None, errorfile=None, directory=outdir)
-    return result
+    try:
+        result = pl.processes_file(filename, outfile=None, errorfile=None, directory=outdir)
+        return result
+    except:
+        return False
 
 def get_txt_files(directory):
     txt_files = []
@@ -24,10 +27,17 @@ def get_txt_files(directory):
             
     return txt_files
 
-umlfiles = get_txt_files(get_current_directory()+"/memory")
+memory_files = get_txt_files(get_current_directory()+"/memory")
+pilote_files = get_txt_files(get_current_directory()+"/pilote")
 
-for umlfile in umlfiles :
-    if generate_images(get_current_directory()+"/memory"+ "/" + umlfile , get_current_directory()):
-        pr_green(umlfile)
+for memory_file in memory_files :
+    if generate_images(get_current_directory()+"/memory"+ "/" + memory_file , get_current_directory()):
+        pr_green(memory_file)
     else:
-        pr_red(umlfile)
+        pr_red(memory_file)
+
+for pilote_file in pilote_files :
+    if generate_images(get_current_directory()+"/pilote"+ "/" + pilote_file , get_current_directory()):
+        pr_green(pilote_file)
+    else:
+        pr_red(pilote_file)
